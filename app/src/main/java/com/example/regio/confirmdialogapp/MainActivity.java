cc1e7748ac96f6003af9e706071125ef556acfdd
albertoregio <albertoregio@yahoo.com.br>
2018-07-14 16:44:47 -0300

    Initial commit

---
+++ app/src/main/java/com/example/regio/confirmdialogapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.regio.confirmdialogapp;
+
+import android.content.DialogInterface;
+import android.location.Address;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Title");
+                alert.setMessage("Confirm?");
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "YES", Toast.LENGTH_LONG).show();
+                    }
+                });
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "NO", Toast.LENGTH_LONG).show();
+                    }
+                });
+                alert.show();
+            }
+        });
+    }
+}
